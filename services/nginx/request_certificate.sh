974b67ee9a08916ec9036d959fb4fc7927512a69
binarymason <john@johnmason.io>
2018-05-22 16:04:55 -0400

    use webroot letsencrypt

--- services/nginx/request_certificate.sh
+++ services/nginx/request_certificate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-docker run -it --rm -p 1086:80 --name letsencrypt \
-  -v "/etc/letsencrypt:/etc/letsencrypt" \
-  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-  quay.io/letsencrypt/letsencrypt:latest auth --server https://acme-staging.api.letsencrypt.org/directory \
-  --debug --renew-by-default --standalone-supported-challenges http-01 \
-  --verbose -d api.charlottedevs.com
+mkdir -p /tmp/letsencrypt/www
+docker run -it --rm --name letsencrypt \
+    -v "/etc/letsencrypt:/etc/letsencrypt" \
+    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+    -v "/tmp/letsencrypt/www:/var/www" \
+    quay.io/letsencrypt/letsencrypt:latest auth --authenticator webroot --webroot-path /var/www --renew-by-default --server \
+    https://acme-v01.api.letsencrypt.org/directory -d api.charlottedevs.com
