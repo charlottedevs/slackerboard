e83c4f113f3c6204c5c8eaef6a9efa6a8987885d
binarymason <john@johnmason.io>
2018-05-24 10:13:56 -0400

    update fetch slack user service

--- services/rails_server/app/services/fetch_slack_user.rb
+++ services/rails_server/app/services/fetch_slack_user.rb
@@ -3,14 +3,13 @@ class FetchSlackUser
   SLACK_API = "https://slack.com/api/users.info"
 
   def call
-    context.user = User.find_or_create_by(slack_identifier: context.slack_identifier) do |user|
-      user_data = JSON.parse(res.body).fetch('user') { Hash.new }
+    context.user = User.find_or_create_by(slack_identifier: slack_identifier) do |user|
+      user_data = JSON.parse(res.body).fetch('user')
       user.real_name = user_data.dig('real_name')
       user.slack_handle = user_data.dig('profile', 'display_name')
       user.profile_image = user_data.dig('profile', 'image_512')
     end
   end
-
 
   private
 
@@ -19,7 +18,11 @@ class FetchSlackUser
   end
 
   def slack_url
-    "#{SLACK_API}?token=#{token}&user=#{context.slack_identifier}"
+    "#{SLACK_API}?token=#{token}&user=#{slack_identifier}"
+  end
+
+  def slack_identifier
+    context.slack_identifier
   end
 
   def token
