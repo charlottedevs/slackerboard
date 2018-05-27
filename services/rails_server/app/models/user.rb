396f6b33bf819cba1dab7140aa33d31942173a7f
binarymason <john@johnmason.io>
2018-05-27 11:29:49 -0400

    use slack_reactions as tie breaker

--- services/rails_server/app/models/user.rb
+++ services/rails_server/app/models/user.rb
@@ -3,14 +3,23 @@ class User < ApplicationRecord
   has_many :slack_reactions
   has_many :slack_channels, through: :slack_messages
 
-  scope :by_slack_messages, -> { order('slack_messages_count DESC') }
-  scope :by_slack_reactions, -> { order('slack_reactions_count DESC') }
+  scope :by_slack_messages, lambda {
+    order(
+      'slack_messages_count DESC, slack_reactions_count DESC, slack_handle'
+    )
+  }
+
+  scope :by_slack_reactions, lambda {
+    order(
+      'slack_reactions_count DESC, slack_messages_count DESC, slack_handle'
+    )
+  }
 
 
   def self.slackers
     User.left_outer_joins(:slack_messages, :slack_reactions)
         .group('users.id')
-        .having('count(slack_messages.user_id) > 0 OR count(slack_reactions.user_id) > 0')
+        .having('count(slack_messages.user_id) > 0')
         .by_slack_messages
         .limit(50)
   end
