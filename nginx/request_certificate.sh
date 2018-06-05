fc36579aab4a2527439dfca9f4a81290b73b7011
binarymason <john@johnmason.io>
2018-06-05 12:23:08 -0400

    update bin/setup to working letsencrypt

--- nginx/request_certificate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p /tmp/letsencrypt/www
-docker run -it --rm --name letsencrypt \
-    -v "/etc/letsencrypt:/etc/letsencrypt" \
-    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-    -v "/tmp/letsencrypt/www:/var/www" \
-    quay.io/letsencrypt/letsencrypt:latest auth --authenticator webroot --webroot-path /var/www --renew-by-default --server \
-    https://acme-v01.api.letsencrypt.org/directory -d api.charlottedevs.com
